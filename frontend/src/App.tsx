e57b37f321beca84964116e0535795cdcb80fd1f
Maksim Ilmast <maksim.ilmast@yandex.com>
2023-03-23 16:45:08 +0200

    html markup ready

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className='Wrapper'>
+      <header className='LogoWrapper'>
+        <span className='Logo'>Northwind</span>
       </header>
-    </div>
+      <main>
+        <section className='SearchWrapper'>
+          <figure className='SearchContainer'>
+            <label htmlFor='SearchBar' className='SearchBarLabel'>Filter orders by product name</label>
+            <input type='text' name='productName' id='SearchBar' placeholder='Aniseed Syrup' />
+          </figure>
+          <figure className='CheckboxContainer'>
+            <label className="CheckboxLabel">Show only shipped orders
+              <input type="checkbox" id='CheckboxShipOrders' />
+              <span className="Checkmark"></span> 
+            </label>
+          </figure>
+        </section>
+        <section className='OrdersWrapper'>
+          <ul className='OrdersList'>
+            <li className='OrdersListItem'>
+              <figure className='OrdersListItemFigure'>
+                <span>#1</span>
+              </figure>
+              <figure className='OrdersListItemFigure'>
+                <h2>Shipping address</h2>
+                <p>59 rue de l-Abbaye</p>
+                <p>Reims 51100</p>
+                <p>France</p>
+              </figure>
+              <figure className='OrdersListItemFigure'>
+                <h2>Customer name</h2>
+                <p>Maria Anders</p>
+              </figure>
+              <figure className='OrdersListItemFigure'>
+                <h2>Products</h2>
+                <p>Chef Antons Cajun Seasoning</p>
+                <p>Chef Antons Cajun Seasoning</p>
+                <p>Chef Antons Cajun Seasoning</p>
+                <p>+ 4 more...</p>
+              </figure>
+              <figure className='OrdersListItemFigure'>
+                <button>View Details</button>
+              </figure>
+            </li>
+            <li className='OrdersListItem'>
+              <figure className='OrdersListItemFigure'>
+                <span>#2</span>
+              </figure>
+              <figure className='OrdersListItemFigure'>
+                <h2>Shipping address</h2>
+                <p>59 rue de l-Abbaye</p>
+                <p>Reims 51100</p>
+                <p>France</p>
+              </figure>
+              <figure className='OrdersListItemFigure'>
+                <h2>Customer name</h2>
+                <p>Maria Anders</p>
+              </figure>
+              <figure className='OrdersListItemFigure'>
+                <h2>Products</h2>
+                <p>Chef Antons Cajun Seasoning</p>
+                <p>Chef Antons Cajun Seasoning</p>
+                <p>Chef Antons Cajun Seasoning</p>
+                <p>+ 4 more...</p>
+              </figure>
+              <figure className='OrdersListItemFigure'>
+                <button>View Details</button>
+              </figure>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </section>
   );
 }
 
