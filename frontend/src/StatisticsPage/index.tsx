dac30f01833ec05da94f0188ebe21ec758b6adf5
Maksim Ilmast <maksim.ilmast@yandex.com>
2023-10-03 19:02:57 +0300

    set up backend for preparing the data for charts and front to fetching and processing the data as well as got first charts properly visualized

--- frontend/src/StatisticsPage/index.tsx
+++ frontend/src/StatisticsPage/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navigation from "../components/Navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
-import LineAreaChart from "../components/charts/LargeAreaChart";
+import Calendar from "../components/charts/Calendar";
+import { useNavigate } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import axios from "axios";
+import { Pie } from "@nivo/pie";
 
 const Wrapper = styled.section`
   margin: 5em auto 0 auto;
@@ -36,25 +40,26 @@ const StatisticsGridWrapper = styled.div`
 `;
 
 const StatisticsGridItem = styled.div`
-  background: black;
+  background: white;
   width: 90%;
   max-width: 600px;
   height: 400px;
   margin: 2em auto;
-  -webkit-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3),
-    0 0 40px rgba(0, 0, 0, 0.1) inset;
-  -moz-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3),
-    0 0 40px rgba(0, 0, 0, 0.1) inset;
-  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+  -webkit-box-shadow: 0 1px 4px rgba(255, 255, 255, 0.3),
+    0 0 40px rgba(255, 255, 255, 0.1) inset;
+  -moz-box-shadow: 0 1px 4px rgba(255, 255, 255, 0.3),
+    0 0 40px rgba(255, 255, 255, 0.1) inset;
+  box-shadow: 0 1px 4px rgba(255, 255, 255, 0.3),
+    0 0 40px rgba(255, 255, 255, 0.1) inset;
   position: relative;
   &:before,
   &:after {
     content: "";
     position: absolute;
     z-index: -1;
-    -webkit-box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
-    -moz-box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
+    -webkit-box-shadow: 0 0 20px rgba(255, 255, 255, 0.8);
+    -moz-box-shadow: 0 0 20px rgba(255, 255, 255, 0.8);
+    box-shadow: 0 0 20px rgba(255, 255, 255, 0.8);
     top: 0;
     bottom: 0;
     left: 10px;
@@ -74,6 +79,52 @@ const StatisticsGridItem = styled.div`
 `;
 
 const StatisticsPage = () => {
+  const [
+    numberOfOrdersThroughoutTimelineData,
+    setNumberOfOrdersThroughoutTimelineData,
+  ] = useState<any>(null);
+  const [numberOfOrdersByCountry, setNumberOfOrdersByCountry] =
+    useState<any>(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedInNorthwindUser");
+    if (!loggedUserJSON) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    const fetchOrdersList = async () => {
+      try {
+        const { data: statisticsData } = await axios.get<any>(
+          `${apiBaseUrl}/statistics/number-of-orders-throughout-timeline`
+        );
+        setNumberOfOrdersThroughoutTimelineData(statisticsData);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    if (!numberOfOrdersThroughoutTimelineData) {
+      void fetchOrdersList();
+    }
+  }, [numberOfOrdersThroughoutTimelineData]);
+
+  useEffect(() => {
+    const fetchOrdersList = async () => {
+      try {
+        const { data: statisticsData } = await axios.get<any>(
+          `${apiBaseUrl}/statistics/number-of-orders-by-country`
+        );
+        setNumberOfOrdersByCountry(statisticsData);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    if (!numberOfOrdersByCountry) {
+      void fetchOrdersList();
+    }
+  }, [numberOfOrdersByCountry]);
+
   return (
     <React.Fragment>
       <Navigation authUserNavToBeDisplayed />
@@ -82,13 +133,35 @@ const StatisticsPage = () => {
         <StatisticsStatus>Statistics</StatisticsStatus>
         <StatisticsGridWrapper>
           <StatisticsGridItem>
-            <LineAreaChart />
+            {numberOfOrdersThroughoutTimelineData && (
+              <Calendar data={numberOfOrdersThroughoutTimelineData} />
+            )}
           </StatisticsGridItem>
-          <StatisticsGridItem />
-          <StatisticsGridItem />
-          <StatisticsGridItem />
-          <StatisticsGridItem />
-          <StatisticsGridItem />
+          <StatisticsGridItem>
+            {numberOfOrdersByCountry && (
+              <Pie data={numberOfOrdersByCountry} height={400} width={580} />
+            )}
+          </StatisticsGridItem>
+          <StatisticsGridItem>
+            {numberOfOrdersThroughoutTimelineData && (
+              <Calendar data={numberOfOrdersThroughoutTimelineData} />
+            )}
+          </StatisticsGridItem>
+          <StatisticsGridItem>
+            {numberOfOrdersThroughoutTimelineData && (
+              <Calendar data={numberOfOrdersThroughoutTimelineData} />
+            )}
+          </StatisticsGridItem>
+          <StatisticsGridItem>
+            {numberOfOrdersThroughoutTimelineData && (
+              <Calendar data={numberOfOrdersThroughoutTimelineData} />
+            )}
+          </StatisticsGridItem>
+          <StatisticsGridItem>
+            {numberOfOrdersThroughoutTimelineData && (
+              <Calendar data={numberOfOrdersThroughoutTimelineData} />
+            )}
+          </StatisticsGridItem>
         </StatisticsGridWrapper>
       </Wrapper>
     </React.Fragment>
---
+++ frontend/src/components/charts/Calendar.tsx
@@ -0,0 +1,30 @@
+import { ResponsiveCalendar } from "@nivo/calendar";
+
+const Calendar = ({ data }: any) => (
+  <ResponsiveCalendar
+    data={data}
+    from={data[0]["day"]}
+    to={data[data.length - 1]["day"]}
+    emptyColor="#eeeeee"
+    colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+    margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+    yearSpacing={40}
+    monthBorderColor="#ffffff"
+    dayBorderWidth={2}
+    dayBorderColor="#ffffff"
+    legends={[
+      {
+        anchor: "bottom-right",
+        direction: "row",
+        translateY: 36,
+        itemCount: 4,
+        itemWidth: 42,
+        itemHeight: 36,
+        itemsSpacing: 14,
+        itemDirection: "right-to-left",
+      },
+    ]}
+  />
+);
+
+export default Calendar;
